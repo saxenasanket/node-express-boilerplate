2ee74c8870ba63c2fc71eb92cb9e2f2420d5f3b9
saxenasanket <sanket.saxena@pixxel.co.in>
2021-05-15 18:56:27 +0530

    first commit

---
+++ src/config/logger.js
@@ -0,0 +1,37 @@
+const winston = require('winston');
+const config = require('./config');
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'white',
+};
+
+winston.addColors(colors);
+
+const logger = winston.createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    config.env === 'development' ? winston.format.colorize({ all: true }) : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    }),
+    new winston.transports.File({ filename: 'logs/all.log' }),
+  ],
+});
+
+module.exports = logger;
---
+++ src/middlewares/auth.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const { roleRights } = require('../config/roles');
+
+// makes sure that there is JWT and user has access for the transaction ...
+
+const verifyCallback = (req, resolve, reject, requiredRights) => {
+  console.log('test2');
+  async (err, user, info) => {
+    console.log(err, user, info);
+    if (err || info || !user) {
+      return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+    }
+    req.user = user;
+
+    if (requiredRights.length) {
+      const userRights = roleRights.get(user.role);
+      const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
+      if (!hasRequiredRights && req.params.userId !== user.id) {
+        return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
+      }
+    }
+
+    resolve();
+  };
+};
+
+const auth = (...requiredRights) => async (req, res, next) => {
+  return new Promise((resolve, reject) => {
+    console.log('coming');
+
+    // reject('rejected');
+    passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
+  })
+    .then(() => next())
+    .catch((err) => next(err));
+};
+
+module.exports = auth;
